38597e33628c1d8896ee23041e96ea18d39a2647
tilensketa <tilen.sketa@gmail.com>
2023-09-20 22:14:18 +0200

    Added libraries

--- Pong/src/main.cpp
+++ Pong/src/main.cpp
@@ -1,6 +1,51 @@
-#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <spdlog/spdlog.h>
 
 int main() {
-	std::cout << "Hello" << std::endl;
-	std::cin.get();
+    glm::vec2 vec(1, 2);
+    spdlog::info("Hello, {}, {}", vec.x, vec.y);
+
+    // GLFW initialization code
+    if (!glfwInit())
+        return -1;
+
+    // GLFW window creation code
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    // Make the window's context current
+    glfwMakeContextCurrent(window);
+
+    // Initialize Glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        // Handle Glad initialization failure
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return -1;
+    }
+
+    // Now you can use OpenGL functions
+    glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+
+    // Loop until the user closes the window
+    while (!glfwWindowShouldClose(window)) {
+        // Render here
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Swap front and back buffers
+        glfwSwapBuffers(window);
+
+        // Poll for and process events
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+    return 0;
 }
--- premake5.lua
+++ premake5.lua
@@ -9,4 +9,38 @@ workspace "Pong"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-include "Pong"
+project "Pong"
+    location "Pong"
+    kind "ConsoleApp"
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files {
+        "%{prj.name}/src/**cpp",
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/vendor/glad/src/glad.c"
+    }
+
+    includedirs {
+        "%{prj.name}/vendor/glad/include",
+        "%{prj.name}/vendor/GLFW/include",
+        "%{prj.name}/vendor/glm",
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    libdirs {
+        "%{prj.name}/vendor/GLFW/lib-vc2022"
+    }
+
+    links {
+        "glfw3.lib",
+        "opengl32.lib"
+    }
+
+    filter "configurations:Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        optimize "on"
